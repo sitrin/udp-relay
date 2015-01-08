66cf769a736a06661c8f9b50aad33df76dafcd22
sitrin <sitrin@users.noreply.github.com>
2015-01-08 21:58:06 +0000

    just getting organized

---
+++ src/client.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include "util.hpp"
+
+int main(int argc, char **argv) {
+  std::cout << "Client is running..." << std::endl;
+}
---
+++ src/server.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include "util.hpp"
+
+int main(int argc, char **argv) {
+  std::cout << "Server is running..." << std::endl;
+}
